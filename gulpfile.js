153fb42f0eee03c21cb6f9f79c7516e4be052b2c
Sagar Lama <sagarlama826@gmail.com>
2019-03-18 03:38:43 +0545

    Gulpfile added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const { src, dest  } = require('gulp');
-const { watch } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const browsersync = require('browser-sync').create();
 const minifyCss = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
@@ -10,30 +10,49 @@ const rename = require('gulp-rename');
 const SCSS_SRC = './scss/**/*.scss';
 const SCSS_DEST = './css';
 
-function compile_sass(){
-		return src(SCSS_SRC)
-		.pipe(sass())
-		.pipe(minifyCss())
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(dest(SCSS_DEST))
-		// .pipe(browserSync.reload({stream:true}));
-		.pipe(browserSync.stream());
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
 }
 
-function serve() {
-		browserSync.init({
-				server:{
-						baseDir:"./"
-				},
-				port:3000
-		});
-}
-function reloadServer() {
-		return src("*.html")
-		.pipe(browserSync.reload());
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
 }
 
-// All events will be watched
-watch(SCSS_SRC,compile_sass,serve, reloadServer)
 
-exports.default = serve;
+function css() {
+  return gulp
+    .src(SCSS_SRC)
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest(SCSS_DEST))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(SCSS_DEST))
+    .pipe(browsersync.stream());
+}
+
+
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
+
+
+// define complex tasks
+const build = gulp.parallel(css);
+const watch = gulp.parallel(watchFiles, browserSync);
+
+
+watch(SCSS_SRC,css,browserSyncReload,watchFiles)
+// export tasks
+exports.css = css;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
